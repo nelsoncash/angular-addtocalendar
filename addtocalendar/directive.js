9236d53a48469858ffa9dec7efb200c59386a6f1
Christopher Marshall <thoughtpalettechris@gmail.com>
2016-09-21 11:34:51 -0500

    Added ngClick func for tracking params

--- addtocalendar/directive.js
+++ addtocalendar/directive.js
@@ -8,6 +8,11 @@
 
 addtocalendar
   .directive('addtocalendar', function () {
+    var ical = "ical";
+    var gcal = "gcal";
+    var outlook = "outlook";
+    var yahoo = "yahoo";
+    var mscal = "mscal";
 
     function getTemplate(prefix) {
       return '\
@@ -18,11 +23,11 @@ addtocalendar
           {{btnText || \'Add to calendar\'}} <span class="caret"></span>\
         </span>\
         <ul class="dropdown-menu">\
-          <li><a ng-click="calendarUrl.dlIcal()" ng-if="calendarUrl.dlIcal">iCalendar</a></li>\
-          <li><a href="{{calendarUrl.google}}" target="_blank">Google Calendar</a></li>\
-          <li><a ng-click="calendarUrl.dlIcal()" ng-if="calendarUrl.dlIcal">Outlook</a></li>\
-          <li><a href="{{calendarUrl.yahoo}}" target="_blank">Yahoo! Calendar</a></li>\
-          <li><a href="{{calendarUrl.microsoft}}" target="_blank">Microsoft Calendar</a></li>\
+          <li><a ng-click="calendarUrl.dlIcal();addToCalTracking(' + ical + ')" ng-if="calendarUrl.dlIcal">iCalendar</a></li>\
+          <li><a href="{{calendarUrl.google}}" ng-click="addToCalTracking(' + gcal + ')" target="_blank">Google Calendar</a></li>\
+          <li><a ng-click="calendarUrl.dlIcal()" ng-click="addToCalTracking(' + outlook + ')" ng-if="calendarUrl.dlIcal">Outlook</a></li>\
+          <li><a href="{{calendarUrl.yahoo}}" ng-click="addToCalTracking(' + yahoo + ')" target="_blank">Yahoo! Calendar</a></li>\
+          <li><a href="{{calendarUrl.microsoft}}" ng-click="addToCalTracking(' + mscal + ')" target="_blank">Microsoft Calendar</a></li>\
         </ul>\
       </div>';
     }
